bc88fdda0234fad9f9ec5e5cfb27ecd15aa2fd81
3onyc <3onyc@x3tech.com>
2014-11-15 14:36:28 +0100

    Make login use layout

--- app/views/auth/login.blade.php
+++ app/views/auth/login.blade.php
@@ -1,24 +1,29 @@
-{{ Form::open(['action' => 'AuthController@login']) }}
-  <p>
-    {{ Form::label('username') }}
-  </p>
-  <p>
-    {{ Form::text('username') }}
-  </p>
-  <p>
-    <small>@if ($errors->has('username')) {{ $errors->first('username') }} @endif</small>
-  </p>
-  <p>
-    {{ Form::label('password') }}
-  </p>
-  <p>
-    {{ Form::password('password') }}
-  </p>
-  <p>
-    <small>@if ($errors->has('password')) {{ $errors->first('password') }} @endif</small>
-  </p>
-  <p>
-    {{ Form::token() }}
-    {{ Form::submit() }}
-  </p>
-{{ Form::close() }}
+@extends('layout.main')
+
+@section('content')
+  <div class="row">
+    <div class="large-offset-4 large-4 small-12 columns">
+      {{ Form::open(['action' => 'AuthController@login']) }}
+        <h1><i class='fi-lock'></i> Login</h1>
+        <p>
+          {{ Form::label('username') }}
+          {{ Form::text('username') }}
+        </p>
+        <p>
+          <small>@if ($errors->has('username')) {{ $errors->first('username') }} @endif</small>
+        </p>
+        <p>
+          {{ Form::label('password') }}
+          {{ Form::password('password') }}
+        </p>
+        <p>
+          <small>@if ($errors->has('password')) {{ $errors->first('password') }} @endif</small>
+        </p>
+        <p>
+          {{ Form::token() }}
+          {{ Form::button('Login', ['type' => 'submit', 'class' => 'expand']) }}
+        </p>
+      {{ Form::close() }}
+    </div>
+  </div>
+@stop
